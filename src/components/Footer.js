a4d8de8f100a16e609833225beda4e6990ee27c4
Codiemaureen <codiestover.cs@gmail.com>
2024-08-02 16:52:26 -0400

    Adding footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+   <footer>
+    <a target="_blank" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
+     <a target="_blank" href="https://icons8.com">Icons8</a>
+   </footer>
+   )
+  }
+
+export default Footer
