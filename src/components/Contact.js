0841d86ea97092898e25db195f6f3909e9b092e5
Codiemaureen <codiestover.cs@gmail.com>
2024-08-14 15:04:00 -0400

    Adding classes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,15 +6,15 @@ const Contact = () => {
         <h2>Contact Form</h2>
         <div className="input-box">
          <label>Name</label>
-         <input type="email" placeholder="Enter Your Name"></input>
+         <input type="email" className="field" placeholder="Enter Your Name"></input>
         </div>
         <div className="input-box">
          <label>Email Address</label>
-         <input type="email" required placeholder="Enter Your Email"></input>
+         <input type="email" required className="field" placeholder="Enter Your Email"></input>
         </div>        
         <div className="input-box">
          <label>Message</label>
-         <textarea name="" id="" placeholder="Enter your message"></textarea>
+         <textarea name="" id="" className="field mess" placeholder="Enter your message"></textarea>
         </div>        
         <button type="submit">Contact Me!</button>
        </form>
