6e8121e6ed023b6748e283d9d753b2c2c60b52a2
Codiemaureen <codiestover.cs@gmail.com>
2024-08-14 14:59:42 -0400

    Adding Contact Form Layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,19 @@ const Contact = () => {
       <section className="contactForm" id="contactForm">
        <form>
         <h2>Contact Form</h2>
-        <input placeholder="Enter Your Name"></input>
+        <div className="input-box">
+         <label>Name</label>
+         <input type="email" placeholder="Enter Your Name"></input>
+        </div>
+        <div className="input-box">
+         <label>Email Address</label>
+         <input type="email" required placeholder="Enter Your Email"></input>
+        </div>        
+        <div className="input-box">
+         <label>Message</label>
+         <textarea name="" id="" placeholder="Enter your message"></textarea>
+        </div>        
+        <button type="submit">Contact Me!</button>
        </form>
       </section>
     </div>
