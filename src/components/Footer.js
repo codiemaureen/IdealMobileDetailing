d85529d5589cdbd553dde7f653347f6ef0c076cb
Codiemaureen <codiestover.cs@gmail.com>
2024-08-14 15:28:06 -0400

    Adding iconCredit div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import Contact from "./Contact"
+import "../style/footer.css"
 
 const Footer = () => {
   return (
    <footer>
     <Contact/>
 
-    <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
-     <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+    <div className="iconCredit">
+      <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
+      <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+     </div>
    </footer>
    )
   }
