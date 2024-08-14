2dee414405c893bfa0bd91acd24065fbba61ba1f
Codiemaureen <codiestover.cs@gmail.com>
2024-08-14 14:53:51 -0400

    Adding contact form

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+const Contact = () => {
+  return (
+    <div>
+      <section className="contactForm" id="contactForm">
+       <form>
+        <h2>Contact Form</h2>
+        <input placeholder="Enter Your Name"></input>
+       </form>
+      </section>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
+import Contact from "./Contact"
+
 const Footer = () => {
   return (
    <footer>
+    <Contact/>
+
     <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
      <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
    </footer>
