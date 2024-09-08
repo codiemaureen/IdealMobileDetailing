b1ecafb4941f0243872fcaa2e4438d9a11777748
Codiemaureen <codiestover.cs@gmail.com>
2024-09-08 18:52:25 -0400

    Editing and adding footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
    <footer>
     <Contact />
-
+    <div className="codieCredit">
+      <span>Website built by <a href="https://codiemaureen.netlify.app/">Codie Maureen</a></span>
+    </div>
     <div className="iconCredit">
       <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
       <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
