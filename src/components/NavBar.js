b2c70e26cc42f76e9fe94a1c13539331a0317a32
Codiemaureen <codiestover.cs@gmail.com>
2024-09-08 20:09:00 -0400

    Fixing broken links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,9 @@ const NavBar = () => {
   return (
    <nav className="nav">
     <ul>
-      <li className="navItems"><a href="/home" className="navLinks">Home</a></li>
+      <li className="navItems"><a href="" className="navLinks">Home</a></li>
       <li className="navItems"><a href="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09" className="navLinks" target="_blank">Schedule Service</a></li>
-      <li className="navItems"><a href="/Photos" className="navLinks">Photos</a></li>
+      <li className="navItems"><a href="" className="navLinks">Photos</a></li>
       <li className="navItems"><a href="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" target="_blank"className="navLinks">Reviews</a></li>      
       <li className="navItems"><a href="#contactForm" className="navLinks">Contact Us</a></li>      
     </ul>
