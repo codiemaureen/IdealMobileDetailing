ba772fa025a5475da8a613af21b7688c80a720e6
Codiemaureen <codiestover.cs@gmail.com>
2024-09-08 20:01:22 -0400

    adding links to each service

--- src/components/Service.js
+++ src/components/Service.js
@@ -4,8 +4,8 @@ import "../style/services.css"
 const Service = ({ service }) => {
   return (
     <section className="service-options">
-      <h2>{service.title}
-      <span>{service.price}</span></h2>
+      <h2><a href="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09" target='_blank'> {service.title}
+      <span>{service.price}</span></a></h2>
       {service.included.map((incServ) => (
        <ul>
         <li key={service.id}>{incServ}</li>
