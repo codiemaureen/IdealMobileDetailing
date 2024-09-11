42a480b618cc67b1b58d82b7d2013a8134bb0eba
Codiemaureen <codiestover.cs@gmail.com>
2024-09-11 13:25:03 -0400

    Adjusting home router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
    <nav className="nav">
     <ul>
-      <li className="navItems"><a href="" className="navLinks">Home</a></li>
+      <li className="navItems"><a href="/" className="navLinks">Home</a></li>
       <li className="navItems"><a href="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09" className="navLinks" target="_blank">Schedule Service</a></li>
       <li className="navItems"><Link to="/photos" className="navLinks">Photos</Link></li>
       <li className="navItems"><a href="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" target="_blank"className="navLinks">Reviews</a></li>      
