f7d9e610d5f8e9d0d8440391cf989be7af0c903b
Codiemaureen <codiestover.cs@gmail.com>
2024-09-18 18:15:49 -0400

    Adding Image Carousel

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,7 +1,11 @@
+import F350DriverInterior from '../images/Before/Supreme F350/driver_interior.jpg';
+import '../style/photos.css';
+
 const Photos = () => {
   return (
-    <div>
+    <div className='image-container'>
       <h1>Before and After Photos</h1>
+      <img className="car-photos" src={F350DriverInterior} alt='F350-driver-interior'/>
     </div>
   )
 }
