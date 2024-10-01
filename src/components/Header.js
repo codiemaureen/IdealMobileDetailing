984d10dc0d1d4e883de438aa5892c2d5a90fa800
Codiemaureen <codiestover.cs@gmail.com>
2024-10-01 14:39:43 -0400

    Adding phone icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import logo from '../images/ideal-mobile-detailing.jpg'
 import NavBar from './NavBar'
 import '../style/header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+
   return (
     <>
     <header className='header'>
       <img src={logo} alt="logo" className='logo'/>
       <h1 className='heading-title'>Ideal Mobile Detailing</h1>
+      <a className='call-button' href='tel:+5869328782'><FontAwesomeIcon className='phone-icon' icon={ faPhone } shake/><span>586.932.8782</span></a>
     </header>
-      <NavBar />
+    <NavBar />
     </>
   )
 }
