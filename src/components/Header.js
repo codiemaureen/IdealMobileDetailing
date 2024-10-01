98fb259d9b1c91ba184588be24a7dd124834b95c
Codiemaureen <codiestover.cs@gmail.com>
2024-10-01 14:51:34 -0400

    Adding country code to phone number

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     <header className='header'>
       <img src={logo} alt="logo" className='logo'/>
       <h1 className='heading-title'>Ideal Mobile Detailing</h1>
-      <a className='call-button' href='tel:+5869328782'><FontAwesomeIcon className='phone-icon' icon={ faPhone } shake/><span>586.932.8782</span></a>
+      <a className='call-button' href='tel:+15869328782'><FontAwesomeIcon className='phone-icon' icon={ faPhone } shake/><span>586.932.8782</span></a>
     </header>
     <NavBar />
     </>
