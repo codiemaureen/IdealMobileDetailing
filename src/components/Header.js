f43eb6cff081b3f983a1c7eafe39e927f070ac79
Codiemaureen <codiestover.cs@gmail.com>
2024-10-01 16:24:53 -0400

    Adding pathnames for links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import NavBar from './NavBar'
 import '../style/header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = () => {
 
@@ -11,7 +14,15 @@ const Header = () => {
     <header className='header'>
       <img src={logo} alt="logo" className='logo'/>
       <h1 className='heading-title'>Ideal Mobile Detailing</h1>
-      <a className='call-button' href='tel:+15869328782'><FontAwesomeIcon className='phone-icon' icon={ faPhone } shake/><span>586.932.8782</span></a>
+      <section className='media-icons'>
+        <div className='social-icons'>
+          <a href='https://www.facebook.com/profile.php?id=100063524828430' target='_blank'><FontAwesomeIcon className='fb-icon' icon= {faFacebook}/></a>
+          <a href='emailto:idealmobile19@gmail.com'><FontAwesomeIcon className='envelope' icon= {faEnvelope}/></a>
+        </div>
+        <div className='contact-anchor'>
+          <a className='call-button' href='tel:+15869328782'><FontAwesomeIcon className='phone-icon' icon={ faPhone } shake/>586.932.8782</a>
+        </div>
+      </section>
     </header>
     <NavBar />
     </>
