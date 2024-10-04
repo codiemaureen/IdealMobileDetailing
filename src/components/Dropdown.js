21d05a5ac4c42433c08cdee745b572c93f278d28
Codiemaureen <codiestover.cs@gmail.com>
2024-10-04 14:19:24 -0400

    Fixing photo link x2

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -19,7 +19,7 @@ const Dropdown = () => {
         <option className="internal" value="/IdealMobileDetailing/index.html">Menu</option>
         <option className="internal" value="/IdealMobileDetailing/index.html">Home</option>
         <option className="external" value="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09">Schedule Service</option>
-        <option className="internal" value="IdealMobileDetailing/index.html/Photos">Photos</option>
+        <option className="internal" value="/IdealMobileDetailing/photos">Photos</option>
         <option className="external" value="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" >Reviews</option>      
         <option className="internal" value="#contactForm">Contact Us</option>         
     </select>
