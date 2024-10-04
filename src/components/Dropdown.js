7b5e14327b9ecd5ca36b9061993ff4b176ada885
Codiemaureen <codiestover.cs@gmail.com>
2024-10-04 14:11:29 -0400

    Fixing photo link

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,9 +5,6 @@ import { Link, Router } from "react-router-dom"
 const Dropdown = () => {
 
     const redirectToURL = () => {
-      let options = document.getElementsByTagName('option')
-      // let external = document.getElementsByClassName('external');
-      // let interal = document.getElementsByClassName('internal');
       let select = document.getElementById("urlSelect");
       let url = select.value;
 
@@ -17,13 +14,14 @@ const Dropdown = () => {
         window.location.href = url;
       }
     }
+
   return (
     <div className="dropdown">
       <select id="urlSelect" onChange={redirectToURL}>
         <option className="internal" value="/IdealMobileDetailing/index.html">Menu</option>
-        <option className="internal" value="/IdealMobileDetailing/index.html"><Link to="/home">Home</Link></option>
+        <option className="internal" value="/IdealMobileDetailing/index.html">Home</option>
         <option className="external" value="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09">Schedule Service</option>
-        <option className="internal"><Link to="/photos" >Photos</Link></option>
+        <option className="internal" value="IdealMobileDetailing/Photos">Photos</option>
         <option className="external" value="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" >Reviews</option>      
         <option className="internal" value="#contactForm">Contact Us</option>         
     </select>
