65d86dd66c4b8bb497709cb38237f24c1e0b9e12
Codiemaureen <codiestover.cs@gmail.com>
2024-10-04 13:59:37 -0400

    Fixing links address internal/external urls

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,24 +5,32 @@ import { Link, Router } from "react-router-dom"
 const Dropdown = () => {
 
     const redirectToURL = () => {
-      var select = document.getElementById("urlSelect");
-      var url = select.value;
-      if (url) {
+      let options = document.getElementsByTagName('option')
+      // let external = document.getElementsByClassName('external');
+      // let interal = document.getElementsByClassName('internal');
+      let select = document.getElementById("urlSelect");
+      let url = select.value;
+
+      if(select.value.includes('https')){
+        window.open(url, '_blank');
+      } else {
         window.location.href = url;
       }
     }
   return (
     <div className="dropdown">
       <select id="urlSelect" onChange={redirectToURL}>
-      <option>Menu</option>
-      <option value="/IdealMobileDetailing/index.html"><Link to="/home" >Home</Link></option>
-      <option value="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09">Schedule Service</option>
-      <option><Link to="/photos" >Photos</Link></option>
-      <option><a href="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" target="_blank">Reviews</a></option>      
-      <option><a href="#contactForm" >Contact Us</a></option>         
+        <option className="internal" value="/IdealMobileDetailing/index.html">Menu</option>
+        <option className="internal" value="/IdealMobileDetailing/index.html"><Link to="/home">Home</Link></option>
+        <option className="external" value="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09">Schedule Service</option>
+        <option className="internal"><Link to="/photos" >Photos</Link></option>
+        <option className="external" value="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" >Reviews</option>      
+        <option className="internal" value="#contactForm">Contact Us</option>         
     </select>
     </div>
   )
 }
+
+
 
 export default Dropdown
