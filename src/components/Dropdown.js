33fb7f14a604e5daa2057fb32db8edf1ec084273
Codiemaureen <codiestover.cs@gmail.com>
2024-10-05 15:27:11 -0400

    Fixing dropdown menu to support, ghpages nav

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,27 +4,16 @@ import { Link } from "react-router-dom";
 
 const Dropdown = () => {
 
-    const redirectToURL = () => {
-      let select = document.getElementById("urlSelect");
-      let url = select.value;
-
-      if(select.value.includes('https')){
-        window.open(url, '_blank');
-      } else {
-        window.location.href = url;
-      }
-    }
-
   return (
-    <div className="dropdown">
-      <select id="urlSelect" onChange={redirectToURL}>
-        <option className="internal" value="/IdealMobileDetailing/index.html">Menu</option>
-        <option className="internal" value="/IdealMobileDetailing/index.html">Home</option>
-        <option className="external" value="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09">Schedule Service</option>
-        <option className="internal" value="/IdealMobileDetailing/photos"><Link to="/photos">Photos</Link></option>
-        <option className="external" value="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" >Reviews</option>      
-        <option className="internal" value="#contactForm">Contact Us</option>         
-    </select>
+    <div class="dropdown">
+      <button className="menu-button">Menu</button>
+      <div class="dropdown-content">
+        <a href="/IdealMobileDetailing/index.html">Home</a>
+        <a href="https://calendly.com/codiemaureen/mom-needs-help?back=1&month=2024-09"  target="_blank">Schedule Service</a>
+        <a href="https://www.facebook.com/profile.php?id=100063524828430&sk=reviews" target="_blank"className="navLinks">Reviews</a>
+        <Link to="/photos" >Photos</Link>
+        <a href="#contactForm" >Contact Us</a>
+      </div>
     </div>
   )
 }
