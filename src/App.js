150a37f18b1a4c87752dba53a0f92a7f8da8d12b
Codiemaureen <codiestover.cs@gmail.com>
2024-10-08 17:50:25 -0400

    code clean up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { useState } from "react";
-import Photos from "./components/Photos";
+import Photos from "./components/Photos"; 
 
 
 const App = () => {
