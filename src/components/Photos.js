b3776e8032f9dba7e1f8ad7dd604474806a32410
Codiemaureen <codiestover.cs@gmail.com>
2024-10-08 14:49:12 -0400

    Seperating before and after image components

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,36 +1,14 @@
-import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
 import '../style/photos.css';
-import { useState } from 'react';
+import AfterPhotos from './AfterPhotos';
 
 
-const Photos = ({ imageUrls }) => {
-  const [imageIndex, setImageIndex] = useState(0);
+const Photos = () => {
 
-  function showPrevImage(){
-    setImageIndex(index => {
-      if(index === 0) return imageUrls.length -1;
-      return index - 1;
-    })
-  }
-
-  function showNextImage() {
-    setImageIndex(index => {
-      if(index === imageUrls.length - 1) return 0;
-      return index + 1;
-    })
-  }
   return (
     <div id="photos" className='before-after-container'>
-      <h1>Before and After Photos</h1>
+      <h1>Gallery</h1>
       <div className='image-container'>
-        <img src={imageUrls[imageIndex]} alt='before photos'/>
-        <div className='overlay'></div>
-        <button onClick={showPrevImage} className='img-slider-btn' style={{ left: 0}}>
-          <ArrowBigLeft />
-        </button>
-        <button onClick={showNextImage} className='img-slider-btn' style={{ right: 0}}>
-          <ArrowBigRight />
-        </button>
+        <AfterPhotos/>
       </div>
     </div>
   )
