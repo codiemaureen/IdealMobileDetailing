7e79bc914569741429b1f1693289a2f1824edd66
Codiemaureen <codiestover.cs@gmail.com>
2024-10-08 19:43:25 -0400

    Adding double tap for mobile

--- src/components/AfterPhotos.js
+++ src/components/AfterPhotos.js
@@ -1,40 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
 import '../style/afterPhotos.css';
+import { faLessThan } from '@fortawesome/free-solid-svg-icons';
 
 
 function importAllImages(r) {
  return r.keys().map(r);
 }
 
-const images = importAllImages(require.context('../images/After', false, /\.(png|jpe?g|svg)$/));
+const afterImages = importAllImages(require.context('../images/After', false, /\.(png|jpe?g|svg)$/));
+const beforeImages = importAllImages(require.context('../images/Before', false, /\.(png|jpe?g|svg)$/));
 
 const AfterPhotos = () => {
 
-  const [photos, setPhotos] = useState([]);
+  const [beforePhotos, setBeforePhotos] = useState([]);
+  const [afterPhotos, setAfterPhotos] = useState([]);
   const [imageIndex, setImageIndex] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
+  const [lastTap, setLastTap] = useState(0);
 
-    function showPrevImage(){
-    setImageIndex(index => {
-      if(index === 0) return images.length -1;
-      return index - 1;
+
+  const handleMouseEnter = () => {
+    setIsHovering(true)
+  }
+  const handleMouseLeave = () => {
+    setIsHovering(false)
+  }
+  const handleDoubleTap = () => {
+    const currentTime = new Date().getTime();
+    const tapLength = currentTime - lastTap;
+
+    if (tapLength < 300 && tapLength > 0) {
+      setIsHovering(!isHovering); // Toggle on double-tap
+    }
+
+    setLastTap(currentTime);
+  }
+
+  function showPrevImage(){
+  setImageIndex(index => {
+    if(index === 0) return afterImages.length -1;
+    return index - 1;
     })
   }
 
   function showNextImage() {
     setImageIndex(index => {
-      if(index === images.length - 1) return 0;
+      if(index === afterImages.length - 1) return 0;
       return index + 1;
     })
   }
 
   useEffect(() => {
-    setPhotos(images);
+    setAfterPhotos(afterImages);
+    setBeforePhotos(beforeImages)
   }, []);
 
   return (
-    <div className="gallery">
-        <img src={photos[imageIndex]} alt={`after-${imageIndex}`}/>
+    <div 
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave} 
+    onTouchStart={handleDoubleTap}
+    className="gallery">
+        {isHovering ? <img src={beforePhotos[imageIndex]} alt={`before-${imageIndex}`}/> :
+        <img src={afterPhotos[imageIndex]} alt={`after-${imageIndex}`}/>}
         <button onClick={showPrevImage} className='img-slider-btn' style={{ left: 0}}>
          <ArrowBigLeft />
         </button>
