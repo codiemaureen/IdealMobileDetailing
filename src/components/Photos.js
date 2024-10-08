97a5167daf82cc463c072dc0fec5c7403a8b8ac5
Codiemaureen <codiestover.cs@gmail.com>
2024-10-08 15:15:11 -0400

    code clean up

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,14 +1,27 @@
 import '../style/photos.css';
 import AfterPhotos from './AfterPhotos';
+import { useState } from 'react';
+import BeforePhotos from './BeforePhotos';
 
 
 const Photos = () => {
+  const [isHovering, setIsHovering] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  }
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  }
   return (
-    <div id="photos" className='before-after-container'>
+    <div 
+    id="photos" 
+    className='before-after-container'
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}>
       <h1>Gallery</h1>
-      <div className='image-container'>
-        <AfterPhotos/>
+      <div>
+        {isHovering ?  <BeforePhotos/> : <AfterPhotos/>}
       </div>
     </div>
   )
