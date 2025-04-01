19b2405b30c577783345802264a7dcd830e00a24
Codiemaureen <codiestover.cs@gmail.com>
2025-04-01 18:46:45 -0400

    adding some instruction for the user

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -8,6 +8,12 @@ const Photos = () => {
       id="photos" 
       className='before-after-container'>
         <h1>Gallery</h1>
+        <p>
+          Take a Look at Some of Our Work!
+        </p>
+        <p>
+          Double Click the Images to Check Out the <em>Before Photo!</em>
+        </p>
         <div className='gallery'>
           <AfterPhotos/>
         </div>
