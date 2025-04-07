24dc9c96b43bbcccc0799d8c311d93dbb8d4d653
Codiemaureen <codiestover.cs@gmail.com>
2025-04-07 10:47:28 -0400

    Scroll to top on header click

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,9 @@ import Dropdown from './Dropdown'
 
 
 const Header = () => {
+  const handleScrollToTop = () => {
+      window.scrollTo(0, 0); 
+  };
 
   return (
     <div className='headerContainer'>
@@ -23,8 +26,8 @@ const Header = () => {
           className='social-icon'>
             <FontAwesomeIcon className='envelope' icon= {faEnvelope}/></a>
         </div>
-        <h1 className='heading-title'>
-          Ideal Mobile Detailing</h1>
+        <h1 className='heading-title' id='heading-title' onClick={handleScrollToTop}>
+          <a href='#heading-title'>Ideal Mobile Detailing</a></h1>
         <div className='social-icons'>
           <a href='tel:+15869328782'><FontAwesomeIcon className={`${'phone-icon'} ${'social-icon'}`}icon={ faPhone } shake/>586.932.8782</a>
         </div>
