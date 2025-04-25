9ca70c1e657144daa0b02b448310a635209ed560
Codiemaureen <codiestover.cs@gmail.com>
2025-04-25 08:56:04 -0400

    Adjusting map size and header font size

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,17 +4,19 @@ import '../style/map.css';
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const GoogleMap = () => (
- <div className='mapContainer'>
-  <h4>Now Servicing the New Port Richey Area!</h4>
-   <APIProvider apiKey={apiKey}>
-     <Map
-       style={{width: '100%', height: '100%'}}
-       defaultCenter={{lat:28.2717 , lng: -82.7195}}
-       defaultZoom={12}
-       gestureHandling={'greedy'}
-       disableDefaultUI={true}
-     />
-   </APIProvider>
+  <div className='mapWrapper'>
+ <h4 className='mapHeader'>Now Servicing the New Port Richey Area!</h4> 
+  <div className='mapContainer'>
+    <APIProvider apiKey={apiKey}>
+      <Map
+        style={{width: '100%', height: '100%'}}
+        defaultCenter={{lat:28.2717 , lng: -82.7195}}
+        defaultZoom={12}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      />
+    </APIProvider>
+    </div>
   </div>
 );
 
