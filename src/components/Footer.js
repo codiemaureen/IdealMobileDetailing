18930d0b0e6e216e7d7e4269b96a231396aa7b63
Codiemaureen <codiestover.cs@gmail.com>
2025-04-28 14:36:18 -0400

    Adding correct url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
    <footer className="footer-container">
     <div className="codieCredit">
-      <span>Website built by <a href="https://codiemaureen.netlify.app/">Codie Groth</a></span>
+      <span>Website built by <a href="https://codiegroth.com">Codie Groth</a></span>
     </div>
     <div className="iconCredit">
       <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
