4f67fcc235a8255e671e0b6db3b1a4014cbf1040
Codiemaureen <codiestover.cs@gmail.com>
2025-05-04 15:06:43 -0400

    Adding before and after to home page

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,19 @@ import '../style/about.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import aboutImg from '../images/pexels-esmihel-14542677.jpg';
+import AfterPhotos from './AfterPhotos';
 
 const About = () => {
   return (
     <div className='aboutContainer' id='aboutContainer'>
-     <img src={aboutImg} alt='aboutImg'className='aboutImg'/>
+      <div >
+        <AfterPhotos className='aboutImg'/>
+      </div>
+      
+     {/* <img src={aboutImg} alt='aboutImg'className='aboutImg'/> */}
      <div className='aboutBlurbContainer'>
       <h2 className='aboutHeader'>About Ideal Mobile Detailing</h2>
+      
       <p className='aboutBlurb'>At Ideal Mobile Detailing, we bring professional-grade car care directly to your driveway. With a passion for perfection and a commitment to convenience, we specialize in restoring that like-new shine—inside and out. Whether you're prepping for a show, maintaining a luxury ride, or just love a spotless car, we deliver personalized detailing services that fit your schedule. Locally owned and operated, we take pride in serving our community with honesty, reliability, and results that speak for themselves.</p>
       <p>
       <a href='tel:+15869328782' className='phoneLink'><FontAwesomeIcon icon={ faPhone } className='phoneIcon' shake/> Get a Free Quote!</a>
