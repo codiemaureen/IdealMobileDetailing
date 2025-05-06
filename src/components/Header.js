beba17cbd3ede38887a3a297552509545881df95
Codiemaureen <codiestover.cs@gmail.com>
2025-05-06 16:41:22 -0400

    Style: Adjusting header text size

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,13 +24,15 @@ const Header = () => {
             <FontAwesomeIcon className='fb-icon' icon= {faFacebook}/></a>
           <a href='mailto:mason@idealmobiledetailing.com'
           className='social-icon'>
-            <FontAwesomeIcon className='envelope' icon= {faEnvelope}/></a>
+            <FontAwesomeIcon className='envelope' icon= {faEnvelope}/>
+          </a>
         </div>
         <h1 className='heading-title' id='heading-title' onClick={handleScrollToTop}>
-          <a href='#heading-title'>Ideal Mobile Detailing</a></h1>
+          <a href='#heading-title' className='heading-title'>Ideal Mobile Detailing</a>
+        </h1>
         <div className='social-icons'>
-          <a href='tel:+15869328782'><FontAwesomeIcon className={`${'phone-icon'} ${'social-icon'}`}icon={ faPhone } shake/>586.932.8782</a>
-        </div>
+          <a href='tel:+15869328782' className={`${'social-icon'}`}><FontAwesomeIcon className={`${'phone-icon'} ${'social-icon'}`}icon={ faPhone } shake/>586.932.8782</a>
+      </div>
       </section>
     </header>
     <Dropdown /> 
