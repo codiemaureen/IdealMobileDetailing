571688f5b45554d2aa0a93894051bf99bfd6fa8e
Codiemaureen <codiestover.cs@gmail.com>
2025-05-12 18:39:39 -0400

    Style: Adding gradient background, centering all text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
   return (
    <footer className="footer-container">
     <div className="codieCredit">
-      <span>Website built by <a href="https://codiegroth.com">Codie Groth</a></span>
+      <span >Website built by <a href="https://codiegroth.com" className="codieCredit">Codie Groth</a></span>
     </div>
     <div className="iconCredit">
       <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/ddI5aoHK2pGh/automatic-car-wash">Automatic Car Wash</a> icon by
       <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
-     </div>
-         <p>
+    </div>
+    <p className="copyright">
       &copy; { currentYear } Ideal Mobile Detailing. All rights reserved.
     </p>
    </footer>
